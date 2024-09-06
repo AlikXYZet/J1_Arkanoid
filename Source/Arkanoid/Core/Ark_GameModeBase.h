2a44faf820698ec3a3f810ba5fb555706916667c
Александр Осипов <alikxyzet@gmail.com>
2024-09-07 00:27:39 +0300

    0.00.00 Base

---
+++ Source/Arkanoid/Core/Ark_GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Ark_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOID_API AArk_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
