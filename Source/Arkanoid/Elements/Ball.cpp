81a3f2f012bd99cedcb199450f5f60d20d1c2e03
Александр Осипов <alikxyzet@gmail.com>
2024-09-08 02:28:05 +0300

    1.00.01 Base Code: Ball

--- Source/Arkanoid/Elements/Ball.cpp
+++ Source/Arkanoid/Elements/Ball.cpp
@@ -1,21 +1,59 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
+// Base:
 #include "Ball.h"
+
+// UE4
+#include "GameFramework/KillZVolume.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Constructors   --- */
 
 // Sets default values
 ABall::ABall()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false; // Предварительно
 
+
+
+	/* ---   Components   --- */
+
+	// Корневой компонент
+	BallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ball Mesh"));
+	RootComponent = BallMesh;
+	BallMesh->SetRelativeScale3D(FVector(0.1f));
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	// Настройки отскока (идеально упругий мяч):
+	ProjectileMovement->bShouldBounce = true;
+	ProjectileMovement->Bounciness = 1.f;
+	ProjectileMovement->Friction = 0.f;
+	// Ограничения движения по плостости (без изменения по Z-оси):
+	ProjectileMovement->bConstrainToPlane = true;
+	ProjectileMovement->SetPlaneConstraintAxisSetting(EPlaneConstraintAxisSetting::Z);
+	//-------------------------------------------
+
+
+
+	/* ---   Предварительные знаячения   --- */
+
+	SetVelocity(500.f);
+	//-------------------------------------------
 }
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Base   --- */
 
 // Called when the game starts or when spawned
 void ABall::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -24,4 +62,31 @@ void ABall::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void ABall::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	// Проверка на AKillZVolume
+	if (Cast<AKillZVolume>(OtherActor))
+	{
+		Destroy();
+	}
+	else
+	{
+		GetVelocity();
+	}
+}
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Velocity   --- */
+
+void ABall::SetVelocity(const float& iNewVelocity)
+{
+	ProjectileMovement->InitialSpeed = iNewVelocity;
+}
+//--------------------------------------------------------------------------------------
+
 
--- Source/Arkanoid/Elements/Ball.h
+++ Source/Arkanoid/Elements/Ball.h
@@ -1,26 +1,72 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
+// Base:
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+// UE4:
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Generated:
 #include "Ball.generated.h"
+//--------------------------------------------------------------------------------------
+
+
 
 UCLASS()
 class ARKANOID_API ABall : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
+
+	/* ---   Constructors   --- */
+
 	// Sets default values for this actor's properties
 	ABall();
+	//-------------------------------------------
+
+
+
+	/* ---   Base   --- */
 
 protected:
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	//-------------------------------------------
+
+
+
+	/* ---   Components   --- */
+
+	/** Меш визуализации мяча */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* BallMesh = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components, meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+	//-------------------------------------------
+
+
+
+	/* ---   Velocity   --- */
+
+	/** Установка новой скорости мяча
+	* @param	iNewVelocity - Новое значение скорости
+	*/
+	void SetVelocity(const float& iNewVelocity);
+
+	//-------------------------------------------
 
 };
