70df2159a2d4ac5d61897622e854d682793e1291
Александр Осипов <alikxyzet@gmail.com>
2024-09-08 08:02:51 +0300

    1.00.04 Base Code: Block

--- Source/Arkanoid/Elements/Block.cpp
+++ Source/Arkanoid/Elements/Block.cpp
@@ -1,27 +1,112 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
+// Base:
 #include "Block.h"
+
+// Interaction:
+#include "Arkanoid/Tools/MyRandom.h"
+#include "Gift.h"
+#include "Ball.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Constructors   --- */
 
 // Sets default values
 ABlock::ABlock()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false; // Предварительно
 
+
+
+	/* ---   Components   --- */
+
+	// Корневой компонент
+	BlockMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Block Mesh"));
+	RootComponent = BlockMesh;
+	BlockMesh->SetRelativeScale3D(FVector(0.1f, 0.5f, 0.5f));
+	//-------------------------------------------
 }
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Base   --- */
 
 // Called when the game starts or when spawned
 void ABlock::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	BlockMesh->OnComponentHit.AddDynamic(this, &ABlock::OnBlockHit);
+
+	NumLives = GetRandom(0, LivesMaterials.Num() - 1);
+
+	SetBlockMaterial();
 }
 
 // Called every frame
 void ABlock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+//--------------------------------------------------------------------------------------
 
+
+
+/* ---   Destroyed   --- */
+
+void ABlock::OnBlockHit(
+	UPrimitiveComponent* HitComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse,
+	const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("ABlock::OnBlockHit"));
+
+	if (OtherActor && Cast<ABall>(OtherActor))
+	{
+		SetBlockMaterial();
+	}
+	// PS: Возможно потребуется заменить "Cast" на группирование объектов по коллизии
 }
 
+void ABlock::Destroyed()
+{
+	SpawnGift();
+
+	Super::Destroyed();
+}
+
+void ABlock::SetBlockMaterial()
+{
+	// Проверка на валидность элемента массива
+	while (NumLives >= 0
+		&& LivesMaterials.IsValidIndex(NumLives)
+		&& LivesMaterials[NumLives] == nullptr)
+	{
+		--NumLives;
+	}
+
+	// Уничтожить или сменить материал
+	if (NumLives < 0)
+	{
+		Destroy();
+	}
+	else
+	{
+		BlockMesh->SetMaterial(0, LivesMaterials[NumLives]);
+		--NumLives;
+	}
+}
+
+void ABlock::SpawnGift()
+{
+	GetWorld()->SpawnActor<AGift>(GetActorLocation(), FRotator());
+
+	// PS: Требуется добавить шанс появления того или иного бонуса из конкретного списка
+}
+//--------------------------------------------------------------------------------------
--- Source/Arkanoid/Elements/Block.h
+++ Source/Arkanoid/Elements/Block.h
@@ -1,26 +1,99 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
+// Base:
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+// Generated:
 #include "Block.generated.h"
+//--------------------------------------------------------------------------------------
+
+
 
 UCLASS()
 class ARKANOID_API ABlock : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
+
+	/* ---   Constructors   --- */
+
 	// Sets default values for this actor's properties
 	ABlock();
+	//-------------------------------------------
+
+
 
 protected:
+
+	/* ---   Base   --- */
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+
+
+	/* ---   Destroyed   --- */
+
+	/** Вызывается, когда этот субъект явно уничтожается во время игрового процесса или в редакторе, но не вызывается во время потоковой передачи уровней или завершения игрового процесса */
+	virtual void Destroyed() override;
+
+	/** Установить материал блока согласно его уровню жизни
+	@note	Запускает уничтожение Блока при Количестве жизней меньше 0
+	*/
+	void SetBlockMaterial();
+
+	/** Создаёт подарок (бонус/антибонус) */
+	void SpawnGift();
+	//-------------------------------------------
+
+
+
+public:
+
+	/* ---   Base   --- */
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	//-------------------------------------------
 
+
+
+	/* ---   Components   --- */
+
+	/** Меш визуализации мяча */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* BlockMesh = nullptr;
+	//-------------------------------------------
+
+
+
+	/* ---   Destroyed   --- */
+
+	// Массив материалов для каждого Уровня жизни блока
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Block", NoClear)
+	TArray<UMaterial*> LivesMaterials;
+
+	/**	Событие, когда этот субъект соприкасается с другим */
+	UFUNCTION()
+	virtual void OnBlockHit(
+		class UPrimitiveComponent* HitComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit);
+	//-------------------------------------------
+
+
+
+private:
+
+	/* ---   Destroyed   --- */
+
+	// Количество "жизней" блока
+	int32 NumLives = -1;
+	//-------------------------------------------
 };
