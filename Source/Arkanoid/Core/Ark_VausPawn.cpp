8ccd70295bdb025c417fae00d092bb136a8c0400
Александр Осипов <alikxyzet@gmail.com>
2024-09-08 05:03:23 +0300

    1.00.03 Base Code: VausPawn

--- Source/Arkanoid/Core/Ark_VausPawn.cpp
+++ Source/Arkanoid/Core/Ark_VausPawn.cpp
@@ -1,34 +1,92 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
+// Base:
 #include "Ark_VausPawn.h"
+
+// UE:
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Constructors   --- */
 
 // Sets default values
 AArk_VausPawn::AArk_VausPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false; // Предварительно
 
+
+
+	/* ---   Components   --- */
+
+	// Корневой компонент
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	VausMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Vaus Mesh"));
+	VausMesh->SetupAttachment(RootComponent);
+	VausMesh->SetRelativeLocation(FVector(-460.f, 0.f, 45.f));
+	VausMesh->SetRelativeScale3D(FVector(0.1f, 1.f, 0.4f));
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(RootComponent);
+	Camera->SetRelativeLocation(FVector(-370.f, 0.f, 810.f));
+	Camera->SetRelativeRotation(FRotator(-70.f, 0.f, 0.f));
+	//-------------------------------------------
+
+
+
+	/* ---   Предварительные значения   --- */
+
+	// Автоподхват игрока
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	// Warning: Данный способ дважды ввызывает PossessedBy(*)
+	//-------------------------------------------
 }
 
-// Called when the game starts or when spawned
+
+
+/* ---   Base   --- */
+
 void AArk_VausPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
 void AArk_VausPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
+//--------------------------------------------------------------------------------------
 
-// Called to bind functionality to input
+
+
+/* ---   Input   --- */
+
 void AArk_VausPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	// Set up gameplay key bindings
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveVaus", this, &AArk_VausPawn::MoveVaus);
 }
 
+void AArk_VausPawn::MoveVaus(const float iValue)
+{
+	if (iValue != 0.f && abs(iValue) <= 1.f)
+	{
+		VausMesh->AddWorldOffset(FVector::YAxisVector * iValue * MoveCoeff, true);
+	}
+}
+
+void AArk_VausPawn::AddMoveCoeff()
+{
+	MoveCoeff *= 1.1f;
+}
+
+void AArk_VausPawn::DecMoveCoeff()
+{
+	MoveCoeff /= 1.1f;
+}
+//--------------------------------------------------------------------------------------
--- Source/Arkanoid/Core/Ark_VausPawn.h
+++ Source/Arkanoid/Core/Ark_VausPawn.h
@@ -1,10 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
+// Base:
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+
+// Generated:
 #include "Ark_VausPawn.generated.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Pre-declaration of classes   --- */
+
+class UCameraComponent;
+//--------------------------------------------------------------------------------------
+
+
 
 UCLASS()
 class ARKANOID_API AArk_VausPawn : public APawn
@@ -12,18 +25,75 @@ class ARKANOID_API AArk_VausPawn : public APawn
 	GENERATED_BODY()
 
 public:
+
+	/* ---   Constructors   --- */
+
 	// Sets default values for this pawn's properties
 	AArk_VausPawn();
+	//-------------------------------------------
+
+
 
 protected:
+
+	/* ---   Base   --- */
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	//-------------------------------------------
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
+
+	/* ---   Input   --- */
+
+	/** Позволяет пешке настраивать пользовательские привязки ввода. Вызывается при владении устройством PlayerController с использованием InputComponent, созданного CreatePlayerInputComponent(). */
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/** Смещение каретки вдоль оси Y с учётом коллизии и Коэффициентом перемещения */
+	void MoveVaus(const float iValue);
+	//-------------------------------------------
+
+
+
+public:
+
+	/* ---   Base   --- */
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	//-------------------------------------------
+
+
+
+	/* ---   Components   --- */
+
+	/** Меш визуализации каретки */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* VausMesh = nullptr;
+
+	/** Камера Игрока */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera = nullptr;
+	//-------------------------------------------
+
+
+
+	/* ---   Input   --- */
+
+	/** Увеличить Коэффициент перемещения на 10% */
+	void AddMoveCoeff();
+
+	/** Уменьшить Коэффициент перемещения на 10% */
+	void DecMoveCoeff();
+	//-------------------------------------------
+
+
+
+private:
+
+	/* ---   Input   --- */
+
+	// Коэффициент перемещения каретки
+	float MoveCoeff = 2;
+	//-------------------------------------------
 };
