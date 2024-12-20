3f99d1c5496bdda1c2cd0714942781ba6591dd6e
Александр Осипов <alikxyzet@gmail.com>
2024-12-20 18:53:06 +0300

    3.02.04 Minor Fixes

--- Source/Arkanoid/Generators/GeneratorBase.cpp
+++ Source/Arkanoid/Generators/GeneratorBase.cpp
@@ -9,7 +9,7 @@
 AGeneratorBase::AGeneratorBase()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false; // Предварительно
+	PrimaryActorTick.bCanEverTick = false; // РџСЂРµРґРІР°СЂРёС‚РµР»СЊРЅРѕ
 
 }
 //--------------------------------------------------------------------------------------
